bfbc958d23e5122ae06e832dd3ec40d086d794ff
kopmer <90770063+kopmer@users.noreply.github.com>
2025-02-09 14:37:52 +0900

    [Silver III] Title: 피보나치 함수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,13 +1,14 @@
 import sys
-N = int(sys.stdin.readline())
- 
-for _ in range(N): 
-    cnt_0 = [1,0]   # fib(0) = 1 0 
-    cnt_1 = [0,1]   # fib(1) = 0 1
-    n = int(sys.stdin.readline())
-    if n>1:
-        for i in range(n-1):
-            cnt_0.append(cnt_1[-1]) # 0 호출 횟수 = fib(n-1)의 1 호출횟수
-            cnt_1.append(cnt_0[-2]+cnt_1[-1])   # 1 호출 횟수 = fib(n-2)의 0호출횟수 + fib(n-1)의 1호출횟수
- 
-    sys.stdout.write(f"{cnt_0[n]} {cnt_1[n]}\n")
+testcase = int(sys.stdin.readline())
+for i in range(testcase):
+    N = int(sys.stdin.readline())
+    dp = [[0] * 2 for _ in range(N+1)]
+    dp[0][0] = 1
+    dp[0][1] = 0
+    if N>0:
+        dp[1][0] = 0
+        dp[1][1] = 1
+        for i in range(2,N+1):
+            dp[i][0] = dp[i-1][0] + dp[i-2][0] 
+            dp[i][1] = dp[i-1][1] + dp[i-2][1] 
+    sys.stdout.write(f"{dp[N][0]} {dp[N][1]}\n")

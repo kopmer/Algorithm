6ebab46a31786a194c613acd3bc3590b65c372fe
kopmer <90770063+kopmer@users.noreply.github.com>
2025-02-11 01:11:17 +0900

    Update 최소 힙.py
    주석추가

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -14,10 +14,7 @@ class min_heap:
         self.items.append(x)
         idx = len(self.items) - 1
         while idx > 0:
-            if idx % 2 == 0:
-                parent_idx = idx//2 - 1
-            else:
-                parent_idx = idx//2
+            parent_idx = (idx-1)//2
             if self.items[parent_idx] > self.items[idx]:    # 부모가 더 크면 교체
                 self.swap(idx,parent_idx)
                 idx = parent_idx    # 현재 인덱스 = 부모 인덱스
@@ -29,18 +26,17 @@ class min_heap:
         if len(self.items) == 0:
             return 1
     def sort(self,idx):
-        while idx*2+1 < len(self.items):
+        while idx*2+1 < len(self.items):   # 왼쪽 자식이 없으면 중단
             left = idx*2 + 1
             right = idx*2 + 2
-            min_idx = left
-
-            if right < len(self.items) and self.items[right] < self.items[left]: # 오른쪽 자식과 왼쪽 자식중 더 작은 인덱스
+            min_idx = left  # 왼쪽이 더작다고 초기화
+            if right < len(self.items) and self.items[right] < self.items[left]: # 오른쪽 자식이 있는지 여부 확인후 오른쪽 자식과 왼쪽자식 비교
                  min_idx = right
 
             if self.items[min_idx] < self.items[idx]:   # 자식이 더 작으면 swap
                 self.swap(idx,min_idx)
                 idx = min_idx
-            else:
+            else:   # 자식이 크면 sort 중단
                 break
 testcase = int(sys.stdin.readline())
 heap = min_heap()

929c193a7f9d5b4180064f830f25e999e12c4a60
kopmer <90770063+kopmer@users.noreply.github.com>
2025-09-02 01:19:42 +0900

    [Gold III] Title: 게리맨더링, Time: 120 ms, Memory: 14896 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -15,95 +15,88 @@ public class Main {
  * 두 선거구의 인구 차이를 최소화
  * 백준시의 정보가 주어졌을떄, 인구차이의 최솟값을 구해보자
  */
+	static int N, minGap;
+	static boolean[] visited, connect;
+	static List<LinkedList<Integer>> graph;
 	static int[] popu;
-	static boolean visited[];//bfs용
-	static boolean used[];//부분집합용
-	static int minGap;
-	static List<LinkedList<Integer>> list;
 	static List<Integer> Alist, Blist;
-	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		popu = new int[N+1];//1base
+		N = Integer.parseInt(br.readLine());
+		
+		graph = new LinkedList<>();
+		
+		popu = new int[N+1];
+		
+		for(int i = 0; i <= N; i++) { //1-base
+			graph.add(new LinkedList<>());
+		}
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 1; i < N+1; i++) {
+		for(int i = 1; i <= N; i++) {
 			popu[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		list = new LinkedList<>();//1base 안쓰는 리스트{{},...}
-		list.add(new LinkedList<>());
-		for(int i = 1; i < N+1; i++) {
+		//인접 구역 번호수
+		for(int i = 1; i <= N; i++) { // N번 노드와 인접한 구역들
 			st = new StringTokenizer(br.readLine());
-			int adj = Integer.parseInt(st.nextToken()); //인접 구역 수
-			list.add(new LinkedList<>());
-			for(int j = 0; j < adj; j++) {
-				list.get(i).add(Integer.parseInt(st.nextToken()));
+			int num = Integer.parseInt(st.nextToken()); //인접 노드 개수
+			for(int j = 0; j < num; j++) {
+				graph.get(i).add(Integer.parseInt(st.nextToken()));
 			}
 		}
-		used = new boolean[N+1];
 		minGap = Integer.MAX_VALUE;
-		
-		PS(1,N);
-		
-		
-		System.out.println(minGap == Integer.MAX_VALUE ? -1 : minGap); //갱신 안됐으면 -1 아니면 갱신된 mingap출력
-		
-		
-//		for(int i = 0; i < N+1; i++) {
-//			System.out.println(Arrays.toString(list.get(i).toArray()));
-//		}
+		visited = new boolean[N+1];
+		subset(1);
+		System.out.println(minGap == Integer.MAX_VALUE ? -1 : minGap);
+	}
+	static boolean isConnected(List<Integer> list) {
+		Queue<Integer> q = new LinkedList<>();
+		connect = new boolean[N+1];
+		q.add(list.get(0));
+		connect[list.get(0)] = true;
+		while(!q.isEmpty()) {
+			int now = q.poll();
+			
+			for(int next : graph.get(now)) {
+				if(connect[next]) continue;
+				//다른 구역이면 패스
+				if(!list.contains(next))continue;
+				connect[next] = true;
+				q.add(next);
+			}
+			
+		}
+		for(int num : list) {
+			if(!connect[num]) return false;
+		}
+		return true;
+	}
+	static void subset(int idx) {
+		if(idx == N) {
+			Alist = new LinkedList<>();
+			Blist = new LinkedList<>();
+			for(int i = 1; i <= N; i++) {
+				if(visited[i] == true) Alist.add(i);
+				else {Blist.add(i);}
+			}
+			if(Alist.isEmpty() || Blist.isEmpty()) return;
+			if(!isConnected(Alist) || !isConnected(Blist)) return;
+			int sumA = 0; int sumB = 0;
+			for(int A : Alist) {
+				sumA += popu[A];
+			}
+			for(int B : Blist) {
+				sumB += popu[B];
+			}
+			minGap = Math.min(minGap, Math.abs(sumA - sumB));
+//			System.out.println(Arrays.toString(Alist.toArray()));
+			return;
+		}
+		visited[idx] = true;
+		subset(idx+1);
+		visited[idx] = false;
+		subset(idx+1);
 
 	}
-	private static boolean isConnected(List<Integer> group) {
-	    visited = new boolean[popu.length];
-	    Queue<Integer> q = new LinkedList<>();
-	    q.add(group.get(0));
-	    visited[group.get(0)] = true;
-
-	    int count = 1;
-	    while(!q.isEmpty()) {
-	        int now = q.poll();
-	        for(int next : list.get(now)) {
-	            if(!visited[next] && group.contains(next)) {
-	                visited[next] = true;
-	                q.add(next);
-	                count++;
-	            }
-	        }
-	    }
-
-	    return count == group.size();
-	}
-	private static void PS(int idx,int N) {
-	    if(idx == N+1) {
-	        int Asum=0;
-	        int Bsum=0;
-	        Alist = new LinkedList<>();
-	        Blist = new LinkedList<>();
-	        
-	        for(int i = 1; i < N+1; i++) {
-	            if(used[i]) {	//A구역
-	                Asum += popu[i];
-	                Alist.add(i);
-	            }
-	            else {			//B구역
-	                Bsum += popu[i];
-	                Blist.add(i);
-	            }
-	        }
-
-	        if(!Alist.isEmpty() && !Blist.isEmpty()) {
-	            if(isConnected(Alist) && isConnected(Blist)) {   //연결성 검사
-	                minGap = Math.min(minGap, Math.abs(Asum-Bsum));
-	            }
-	        }
-	        return;
-	    }
-	    used[idx] = true;
-	    PS(idx+1,N);
-	    used[idx] = false;
-	    PS(idx+1,N);
-	}
-
 }

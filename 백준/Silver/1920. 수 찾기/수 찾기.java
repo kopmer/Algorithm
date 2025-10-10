b93e70e0971e8ecdc0a17436052146159951910e
kopmer <90770063+kopmer@users.noreply.github.com>
2025-10-10 11:58:12 +0900

    [Silver IV] Title: 수 찾기, Time: 1332 ms, Memory: 70464 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		HashMap<Integer, Integer> map = new HashMap<>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			map.put(num, i);
+		}
+		
+		
+		int M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < M; i++) {
+			int x = Integer.parseInt(st.nextToken());
+			boolean isExist = map.containsKey(x);
+			System.out.println(isExist ? 1:0);
+		}
+		
+
+	}
+
+}

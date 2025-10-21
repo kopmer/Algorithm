e5045c9cbe0c54da836fcb51b92929146ffd75c0
kopmer <90770063+kopmer@users.noreply.github.com>
2025-10-21 21:36:46 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 144 ms, Memory: 21228 KB -BaekjoonHub

---
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] isBuilding = new int[1000001]; //x에 있는 건물의 높이
+		Stack<Integer> s = new Stack<>();
+		int BuildingCnt = 0;
+		for(int i = 0; i < N; i ++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			// 높이 낮아지면 pop, count++
+			while(!s.isEmpty() && s.peek() > y) {
+				s.pop();
+				BuildingCnt++;
+			}
+			
+			// 더 높으면 push
+			if(s.isEmpty() || s.peek() < y) {
+				s.push(y);
+			}
+			
+		}
+		
+		while(!s.isEmpty()) {
+//			System.out.println(s.peek());
+			int y = s.pop();
+			if(y == 0) continue; // 0은 왜들어가는데;;
+			BuildingCnt++;
+		}
+		System.out.println(BuildingCnt);
+
+	}
+}

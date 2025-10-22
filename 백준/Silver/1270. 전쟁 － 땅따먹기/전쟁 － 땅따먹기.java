732768f208c61815013c366936d746212acff38f
kopmer <90770063+kopmer@users.noreply.github.com>
2025-10-22 17:56:41 +0900

    [Silver III] Title: 전쟁 - 땅따먹기, Time: 2828 ms, Memory: 313280 KB -BaekjoonHub

--- 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.java
+++ 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.java
@@ -1,43 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
-/*
- * 부대에 한 번호가 절반이상 차지하면 그 번호의 지배하에 놓임
- * 각 땅들을 지배한 군대의 번호를 출력
- * 아직 전쟁중이라면 SYJKGW를 출력
- * 해시 <부대 번호, 부대 번호 횟수> 선언하고 번호 누적하면 될듯
- * 처음에 잠깐 잘못생각한 부분 -> 과반수 부대 나왔는데 이거보다 더 많은 부대가 있으면 어떡하지? -> ㅋㅋ 이미 과반수 넘었으니까 그럴일 없음(바보 인증 완료)
- * 새로 알게된 점 : 해시맵 순회 방법 Map.Entry이용
- */
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		for(int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			Long TiCnt = Long.parseLong(st.nextToken());
-			
-			HashMap<Long, Integer> Ti = new HashMap<>(); // <부대 번호, 부대 번호 횟수>
-			
-			for(long j = 0 ; j < TiCnt; j++) {
-				Long k = Long.parseLong(st.nextToken());
-				Ti.put(k, Ti.getOrDefault(k, 0) + 1); // 횟수 누적
-			}
-			
-			long majority = -1;
-			for(Map.Entry<Long, Integer> T : Ti.entrySet()) {
-				if(T.getValue() > TiCnt / 2) {
-					majority = T.getKey();
-					break; //과반수 부대 찾으면 종료
-				}
-			}
-			System.out.println(majority == -1 ? "SYJKGW" : majority);
-		}
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens;
+    static int N, Pnum;
+    static long[] peoples;
 
-	}
+    public static void main(String[] args) {
+        try {
+            N = Integer.parseInt(input.readLine());
 
+            for (int i = 0; i < N; i++) {
+                tokens = new StringTokenizer(input.readLine());
+                Pnum = Integer.parseInt(tokens.nextToken());
+                peoples = new long[Pnum];
+
+                for (int j = 0; j < Pnum; j++) {
+                    peoples[j] = Long.parseLong(tokens.nextToken());
+                }
+
+                long findNum = 0;
+                int cnt = 0;
+                for (int j = 0; j < Pnum; j++) {
+                    if (cnt == 0) {
+                        findNum = peoples[j];
+                        cnt = 1;
+                    } else if (peoples[j] == findNum) {
+                        cnt++;
+                    } else {
+                        cnt--;
+                    }
+                }
+
+                // 과반수인지 확인
+                int realCnt = 0;
+                for (int j = 0; j < Pnum; j++) {
+                    if (peoples[j] == findNum) {
+                        realCnt++;
+                    }
+                }
+
+                if (realCnt > Pnum / 2) {
+                    System.out.println(findNum);
+                } else {
+                    System.out.println("SYJKGW");
+                }
+            }
+        } catch (IOException | NumberFormatException e) {
+            System.err.println("입력 오류 발생: " + e.getMessage());
+        }
+    }
 }

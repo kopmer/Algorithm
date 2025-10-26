a89f67797dd6efb48b042a5aaef18e9d80ffd4b0
kopmer <90770063+kopmer@users.noreply.github.com>
2025-10-26 20:10:56 +0900

    [Silver II] Title: 수 이어 쓰기, Time: 92 ms, Memory: 15668 KB -BaekjoonHub

--- 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
@@ -1,33 +1,36 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 	/*
-	 * 이 수는 최대 3000자리 이다 -> N은 3000까지
+	 * 백준 1515: 수 이어쓰기
 	 */
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String num = br.readLine();
 		char[] numList = num.toCharArray();
-		int idx = 0;
+		int idx = 0; // 찾을수(numList의 target)
 		
-		for(int i = 1;; i++) { // 1 ~ 3000 될수 있는 후보 번호들
-			String possible = "" + i;
-			//현재 번호 될수있으면 다음수 진행
-			char[] posChar = possible.toCharArray();
+		for(int i = 1;; i++) { //이어쓸 수의 후보(1 ~ N)
+			String possible = "" + i; //int -> string
+			
+			//이어쓸 후보 숫자 문자 배열로 변환
+ 			char[] posChar = possible.toCharArray();
 			
 			for(char p : posChar) {
-				if(!(numList[idx] == p)) continue;
-				idx++;
-				if(idx < numList.length) continue;
-				break;
+				//현재 찾는수 아니면 패스
+				if(numList[idx] != p) continue;
+				idx++; //다음 수 찾기(target++)
+				//다 찾았으면 종료
+				if(idx >= numList.length) {
+					System.out.println(i);
+					return;
+				}
 			}
 					
-			if(idx >= numList.length) {
-				System.out.println(i);
-				break;
-			}
+			
 			
 		}
 	}

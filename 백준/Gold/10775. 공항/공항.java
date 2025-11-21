d45a2c8c4cad11753f77c59cf0bd7387d32a588f
kopmer <90770063+kopmer@users.noreply.github.com>
2025-11-21 21:53:26 +0900

    [Gold II] Title: 공항, Time: 200 ms, Memory: 22432 KB -BaekjoonHub

---
+++ 백준/Gold/10775. 공항/공항.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+/*
+ * G개의 게이트(1~G)
+ * P개의 비행기가 순서대로 도착
+ * 어느곳에도 착륙할곳 없으면 폐쇠(중지)
+ * gi 도킹 하면 다음 gi에 접근하는 비행기는 gi - 1로 도킹해야함 gi -> gi - 1
+ */
+	public static int[] gates;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int G = Integer.parseInt(br.readLine()); // 게이트 수
+		int P = Integer.parseInt(br.readLine()); // 비행기 수
+		int cnt = 0; // 착륙 비행기 수
+		
+		// 게이트 도킹 여부(1based)
+		gates = new int[G+1]; 
+		for(int i = 1; i <= G; i++) {
+			gates[i] = i;
+		}
+		
+		for(int i = 0;i < P; i++) {
+			int gi = Integer.parseInt(br.readLine()); // 1 ~ gi
+			if(find(gi) == 0) break; // 도킹할수 없으면 종료
+			cnt++;
+			union(find(gi), find(gi)-1); // 부모게이트 연결
+		}
+		System.out.println(cnt);
+	}
+	// gi -> gi-1
+	public static void union(int a, int b) {
+		a = find(a);
+		b = find(b);
+		gates[a] = b;
+	}
+	// 부모 찾기, 압축
+	public static int find(int x) {
+		if(x == gates[x]) return x;
+		return gates[x] = find(gates[x]);
+	}
+}
+

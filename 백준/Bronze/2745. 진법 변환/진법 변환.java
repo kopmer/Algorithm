f00ab591cdd3913b1a6ecef9130882a4012919d5
kopmer <90770063+kopmer@users.noreply.github.com>
2025-11-21 23:01:40 +0900

    [Bronze II] Title: 진법 변환, Time: 60 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		String B = st.nextToken();
+		int exp = B.length() - 1;
+		int Deci = 0;
+		int N = Integer.parseInt(st.nextToken());
+		for(int x : B.toCharArray()) {
+			if(48 <= x && x < 58) {
+				x -= 48;
+			}
+			else {
+				x -= 55;
+			}
+			Deci += Math.pow(N, exp) * x;
+			exp--;
+		}
+		System.out.println(Deci);
+	}
+
+}

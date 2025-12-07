f1c96467b70cec224d6a902c2c060790a8b374ef
kopmer <90770063+kopmer@users.noreply.github.com>
2025-12-07 12:05:22 +0900

    [Silver II] Title: 연속합, Time: 156 ms, Memory: 21452 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[N+1];
+		int ans = Integer.MIN_VALUE;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= N; i++) {
+			int a = Integer.parseInt(st.nextToken());
+			// 누적 vs 현재 값 비교
+			dp[i] = Math.max(a, dp[i-1] + a);
+			ans = Math.max(dp[i], ans);
+//			System.out.println(dp[i]);
+		}
+		System.out.println(ans);
+	}
+
+}

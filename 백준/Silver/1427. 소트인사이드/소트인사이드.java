eed4014426c1bf8d72a513a3e6fb8103db498d11
kopmer <90770063+kopmer@users.noreply.github.com>
2025-12-15 21:00:57 +0900

    [Silver V] Title: 소트인사이드, Time: 68 ms, Memory: 11560 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String line = br.readLine();
+		char[] arr = line.toCharArray();
+		Arrays.sort(arr);
+		
+		for(int i = arr.length-1; i >= 0; i--) {
+			sb.append(arr[i]);
+		}
+		System.out.println(sb);
+
+	}
+
+}
